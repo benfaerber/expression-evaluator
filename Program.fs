ae092224ecadd884da28e7a50caf6adae6665ac2
Ben Faerber <benfaerber@protonmail.com>
2022-03-02 13:21:14 -0700

    Parsing

--- Program.fs
+++ Program.fs
@@ -4,16 +4,18 @@
   | Times
   | Divide
   | Modulo
+  | Exponent
 
 type token =
   | Number of float
   | Operator of operator
 
-let example = "23+13*23"
+
+let example = "23+13.5*3/4"
 
 let lex_number (s: string) =
   let is_digit = function
-  | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> true
+  | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '.' -> true
   | _ -> false in
 
   let literal = Seq.toList s |> List.unfold (
@@ -35,6 +37,7 @@ let lex_operator (s: string) =
   | '*' -> Some (Operator Times)
   | '/' -> Some (Operator Divide)
   | '%' -> Some (Operator Modulo)
+  | '^' -> Some (Operator Exponent)
   | _ -> None in
 
   match Seq.toList s with
@@ -48,6 +51,63 @@ let token_to_string = function
   | Operator Times -> "Times"
   | Operator Divide -> "Divide"
   | Operator Modulo -> "Module"
+  | Operator Exponent -> "Exponent"
 
-let p, u = (lex_number example)
-printfn "%s" (token_to_string p.Value)
+let lex_token (s: string) =
+  let lexers = [lex_number; lex_operator] in
+  let found_lexer = lexers |> List.find (fun lexer ->
+    match lexer s with
+    | Some _, _ -> true
+    | _ -> false) in
+  found_lexer s
+
+let lex (s: string) =
+  let head = lex_token s in
+  let first_token, _ = head in
+
+  let optional_ast = head |> List.unfold (
+    function
+    | _, "" -> None
+    | _, u ->
+      let next_p, next_u = lex_token u in
+      Some (next_p, (next_p, next_u))) in
+
+  let tail = optional_ast |> List.filter (
+    fun opt -> opt.IsSome) |> List.map (
+      fun opt -> opt.Value) in
+
+  [first_token.Value] @ tail
+
+
+  // (fun acc (p, u) -> acc @ [lex_token u]) (lex_token s)
+
+let tokens = (lex example)
+let print_tokens ast =
+  List.iter (fun t -> printfn "%s" (token_to_string t)) ast
+
+let pemdas = [Exponent; Times; Divide; Plus; Minus]
+
+// type operator =
+//   | Plus
+//   | Minus
+//   | Times
+//   | Divide
+//   | Modulo
+//   | Exponent
+
+
+let apply_operation a b = function
+  | Plus -> a + b
+  | Minus -> a - b
+  | Times -> a * b
+  | Divide -> a / b
+  | Modulo -> a + b
+  | Exponent -> a + b
+
+
+let rec evaluate = function
+  | Number a :: Operator op :: Number b :: tl ->
+    evaluate ([Number (apply_operation a b op)] @ tl)
+  | r -> r
+
+print_tokens (evaluate tokens)
