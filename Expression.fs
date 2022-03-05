8947bae8d5e5cd0c96768961bb36afc8e6cb4b6a
Ben Faerber <benfaerber@protonmail.com>
2022-03-05 12:26:36 -0700

    Added seperate expression module

---
+++ Expression.fs
@@ -0,0 +1,34 @@
+module Expression
+  open Syntax
+
+  let debug_expression_printer exp ast =
+    debug_print "Complete AST:"
+    debug_print "------------"
+    debug_print (sprintf "Raw Expression: %s" exp)
+    print_ast ast
+    debug_print "-------------"
+
+  let evaluate exp =
+    let ast = exp |> Lexer.generate_ast in
+    debug_expression_printer exp ast
+    ast |> Evaluator.evaluate_ast
+
+  let print_result exp =
+    printfn "%f" (exp |> evaluate)
+
+  let repl () =
+    printfn "Expression REPL"
+    let rec aux () =
+      printf "> "
+      let inp = System.Console.ReadLine() in
+
+      match inp with
+      | "" -> aux ()
+      | "q" | "quit" ->
+        printfn "Bye!"
+      | _ ->
+        let solution = evaluate inp in
+        printfn ": %f" solution
+        aux ()
+
+    in aux ()
--- Program.fs
+++ Program.fs
@@ -1,38 +1,4 @@
-﻿open Syntax
-
-module Expression =
-  let debug_expression_printer exp ast =
-    debug_print "Complete AST:"
-    debug_print "------------"
-    debug_print (sprintf "Raw Expression: %s" exp)
-    print_ast ast
-    debug_print "-------------"
-
-  let evaluate exp =
-    let ast = exp |> Lexer.generate_ast in
-    debug_expression_printer exp ast
-    ast |> Evaluator.evaluate_ast
-
-  let print_result exp =
-    printfn "%f" (exp |> evaluate)
-
-  let repl () =
-    printfn "Expression REPL"
-    let rec aux () =
-      printf "> "
-      let inp = System.Console.ReadLine() in
-
-      match inp with
-      | "" -> aux ()
-      | "q" | "quit" ->
-        printfn "Bye!"
-      | _ ->
-        let solution = evaluate inp in
-        printfn "- %f" solution
-        aux ()
-
-    in aux ()
-
+﻿
 let runner () =
   let is_repl_mode = true
   if is_repl_mode then
@@ -42,12 +8,8 @@ let runner () =
     let complex_expression_2 = "73/3+(4^2+(8-3))"
     let complex_expression_3 = "~40+20"
 
-
-    // print_ast (Lexer.generate_ast complex_expression_2)
-    // printfn "%f" (evaluate_expression "23+13.5*3/4")
-    // printfn "%f" (evaluate_expression "8+4-2^4/10")
     printfn "%f" (Expression.evaluate complex_expression_1)
     printfn "%f" (Expression.evaluate complex_expression_2)
-    printfn "%f" (Expression.evaluate "(1+1)")
+    printfn "%f" (Expression.evaluate complex_expression_3)
 
 runner ()
